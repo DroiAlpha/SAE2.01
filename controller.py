c90b5d8747974878ddbb78b5f6b9f87ec3f36592
Clémence LOSA <clemence.losa@etu.u-pec.fr>
2025-06-16 20:55:28 +0200

    feat: mise à jour du controller.py

--- controller.py
+++ controller.py
@@ -1,7 +1,11 @@
+""" Contrôler de l'application Flask pour le site web sur les prélèvements d'eau """
+
 #####################################################################
 # IMPORTATION DES MODULES
 #####################################################################
-# ! Il vous faut installer : pip install flask-caching redis
+
+# ! Installer Flask-Caching depuis le terminal avec la commande : pip install flask-caching redis
+
 from flask import Flask, render_template, request
 import matplotlib
 from flask_caching import Cache
@@ -17,7 +21,7 @@ from model.chroniques import *
 # Déclaration de l'application Flask
 app = Flask(__name__)
 
-# Import le server Redis de la vm 
+# Import le server Redis de la VM 
 app.config['CACHE_TYPE'] = 'RedisCache'
 app.config['CACHE_REDIS_HOST'] = '10.10.41.217'
 app.config['CACHE_REDIS_PORT'] = 6379
@@ -33,6 +37,14 @@ matplotlib.use('Agg')
 def test_cache():
     return f"Time: {time.time()}"
 
+#####################################################################
+# ROUTES
+#####################################################################
+
+################################
+# ACCUEIL
+################################
+
 # Route pour la page d'accueil "index.html"
 @app.route("/")
 @cache.cached(timeout=300)
@@ -41,71 +53,33 @@ def accueil():
     Fonction de définition de l'adresse de la page d'accueil "index.html"
     """
     # Affichage du template
-    return render_template('index.html')
+    return render_template(
+        'index.html', 
+        page_title="Accueil"
+    )
 
-# Route pour la page d'à propos "apropos.html"
-@app.route('/a-propos/manuel-utilisation')
-def a_propos_manuel_utilisation():
-    """
-    Fonction de définition de l'adresse de la page d'à propos "apropos.html"
-    """
-    # Affichage du template
-    return render_template('a-propos-manuel-utilisation.html')
+################################
+# TABLEAU DE BORD
+################################
 
-# Table : “Tableau de bord”
-@app.route('/tableau-bord')
-def tableau_de_bord():
+# Route pour la page de la carte des prélèvements en eau "tab_carte.html"
+@app.route('tableau-bord/carte-prelevements',  methods=['GET', 'POST'])
+def carte():
     """
-    Affiche le template tableau-bord.html
+    Fonction de définition de l'adresse de la page de la carte des prélèvements en eau "tab_carte.html"
+    Affiche la carte des prélèvements d'eau avec les ouvrages
     """
-    return render_template('tableau-bord.html')
+    return render_template(
+        'tab_carte.html', 
+        page_title="Tableau de bord"
+    )
 
-# Page “Jeux de données”
-@app.route('/jeux-de-donnees')
-def jeux_de_donnees():
-    """
-    Affiche le template jeux-de-donnees.html
-    """
-    return render_template('jeux-de-donnees.html')
-
-# Route pour la page d'affichage des tableaux intermédiaires "data_tab.html"
-@app.route('/data_tab')
-# Fonction générique de transmission des valeurs filtrées d'un tableau
-def render_filtered_template(template, filter_fct, form_keys):
-    """
-    Fonction générique de transmission des valeurs filtrées d'un tableau
-    Je dois mettre les données des 3 tableaux (chroniques, pt_prelevement, ouvrage)
-    DANS la fonction
-    """
-    if request.method == 'POST':
-        filters = {key: request.form.get(key) for key in form_keys}
-        filtered_values = filter_fct(filters)
-        return render_template(template, filtered_values=filtered_values, **filters)
-    
-    chroniques = Chroniques()
-
-    tab_chroniques = chroniques.filtre()
-    tab_pt_prelev = db.obtenir_info_prelevement()
-    tab_ouvrages = db.obtenir_info_ouvrage()
-
-    return render_template(template,
-                           tab_chroniques = tab_chroniques,
-                           tab_ouvrages = tab_ouvrages,
-                           tab_pt_prelev = tab_pt_prelev
-                           )
-
-@app.route('/Carte',  methods=['GET', 'POST'])
-def map():
-    """
-    Route permettant d'aller a la carte
-    """
-    return render_template('map.html')
-
-@app.route('/Graphiques', methods=['GET', 'POST'])
+# Route pour la page des graphiques sur les usages de l'eau "tab_usages.html"
+@app.route('/tableau-bord/usages-eau', methods=['GET', 'POST'])
 def graphiques():
     """
-    Route pour afficher histogramme + diagramme circulaire,
-    avec ou sans filtres.
+    Fonction de définition de l'adresse de la page des graphiques sur les usages de l'eau "tab_usages.html"
+    Affiche les graphiques (histogrammes groupés, diagramme en barres empilées et diagramme circulaire) sur les usages de l'eau, avec ou sans filtres
     """
     chroniques = Chroniques()
     
@@ -138,10 +112,11 @@ def graphiques():
         histogramme = sns_horizontalbarplot(filtered_data)
 
         return render_template(
-            'Graphiques.html',
+            'tab_usages.html',
             diagramme_circulaire=diagramme_circulaire,
             histogramme=histogramme,
-            filters=filters  
+            filters=filters, 
+            page_title="Tableau de bord"
         )
 
     # Si GET ou aucun filtre
@@ -149,18 +124,20 @@ def graphiques():
     histogramme = sns_horizontalbarplot(donnees)
     
     return render_template(
-        'Graphiques.html',
+        'tab_usages.html',
         diagramme_circulaire=diagramme_circulaire,
         histogramme=histogramme,
-        filters=None
+        filters=None, 
+        page_title="Tableau de bord"
     )
 
-
-@app.route('/Evolution', methods=['GET', 'POST'])
+# Route pour la page du graphique sur évolution temporelle du volume d'eau prélevé "tab_evolution.html"
+@app.route('/tableau-bord/evolution-temporelle', methods=['GET', 'POST'])
 def evolution():
     """
-    Route pour afficher une courbe d'évolution pour un ouvrage.
-    L'utilisateur choisit un ouvrage, et la courbe montre les volumes selon les années.
+    Route pour la page du graphique sur évolution temporelle du volume d'eau prélevé "tab_evolution.html"
+    Affiche un graphique linéaire multiple sur l'évolution des volumes d'eau prélevés pour un ouvrage spécifique
+    Chaque ouvrage est représenté par une courbe, et l'utilisateur peut choisir l'ouvrage à afficher
     """
 
     chroniques = Chroniques()
@@ -180,62 +157,111 @@ def evolution():
             graphique = sns_courbe(df)
 
     return render_template(
-        'Evolution.html',
+        'tab_evolution.html',
         graphique=graphique,
-        nom_ouvrage=nom_ouvrage
+        nom_ouvrage=nom_ouvrage, 
+        page_title="Tableau de bord"
     )
 
+################################
+# JEUX DE DONNÉES
+################################
 
-# ------- OPTIONNELS ----------#
+# Route pour la page du jeu de données pour les chroniques "jeu_chroniques.html"
+@app.route('/jeux-donnees/chroniques')
+def jeu_chroniques():
+    """
+    Route pour la page du jeu de données pour les chroniques "jeu_chroniques.html"
+    Affiche les données chroniques d'eau prélevée dans un tableau, avec des filtres pour affiner la recherche
+    """
+    render_filtered_template(
+        'jeu_chroniques.html',
+        Chroniques().filtre,
+        form_keys=["annee", "libelle_usage", "nom_commune", "libelle_departement", "nom_ouvrage"]
+    )
+    # Fonction générique de transmission des valeurs filtrées d'un tableau
+    return render_template('jeu_chroniques.html', page_title="Jeux de données")
 
-@app.route('/connexion')
-def connexion():
-    pass
+# Route pour la page du jeu de données pour les points de prélèvement "jeu_points_prelevement.html"
+@app.route('/jeux-donnees/points-prelevement')
+def jeu_points_prelevement():
+    """
+    Route pour la page du jeu de données pour les points de prélèvement "jeu_points_prelevement.html"
+    Affiche les points de prélèvement d'eau dans un tableau, avec des filtres pour affiner la recherche
+    """
+    render_filtered_template(
+        'jeu_points_prelevement.html',
+        Chroniques().filtre,
+        form_keys=["annee", "libelle_usage", "nom_commune", "libelle_departement", "nom_ouvrage"]
+    )
+    # Fonction générique de transmission des valeurs filtrées d'un tableau
+    return render_template('jeu_points_prelevement.html', page_title="Jeux de données")
 
-@app.route('/forum')
-def forum():
-    pass
+# Route pour la page du jeu de données pour les ouvrages "jeu_ouvrages.html"
+@app.route('/jeux-donnees/ouvrages')
+def jeu_ouvrages():
+    """
+    Route pour la page du jeu de données pour les ouvrages "jeu_ouvrages.html
+    Affiche les ouvrages d'eau dans un tableau, avec des filtres pour affiner la recherche
+    """
+    render_filtered_template(
+        'jeu_ouvrages.html',
+        Chroniques().filtre,
+        form_keys=["annee", "libelle_usage", "nom_commune", "libelle_departement", "nom_ouvrage"]
+    )
+    # Fonction générique de transmission des valeurs filtrées d'un tableau
+    return render_template('jeu_ouvrages.html', page_title="Jeux de données")
+
+def render_filtered_template(template, filter_fct, form_keys):
+    """
+    Fonction générique de transmission des valeurs filtrées d'un tableau
+    """
+    #! Il faut encore mettre les données des 3 tableaux (chroniques, pt_prelevement, ouvrages) DANS la fonction
+    if request.method == 'POST':
+        filters = {key: request.form.get(key) for key in form_keys}
+        filtered_values = filter_fct(filters)
+        return render_template(template, filtered_values=filtered_values, **filters)
+    
+    chroniques = Chroniques()
+
+    tab_chroniques = chroniques.filtre()
+    tab_pt_prelev = db.obtenir_info_prelevement()
+    tab_ouvrages = db.obtenir_info_ouvrage()
+
+    return render_template(
+        template,
+        tab_chroniques = tab_chroniques,
+        tab_ouvrages = tab_ouvrages,
+        tab_pt_prelev = tab_pt_prelev
+    )
+
+################################
+# À PROPOS
+################################
+
+# Route pour la page s'à propos sur le manuel d'utilisation de l'application "a_propos_manuel.html"
+@app.route('/a-propos/manuel-utilisation')
+def a_propos_manuel():
+    """
+    Fonction de définition de l'adresse de la page d'à propos sur le manuel d'utilisation de l'application "a_propos_manuel.html"
+    Cette page fournit des informations sur l'utilisation de l'application, les fonctionnalités disponibles et comment naviguer dans le site
+    """
+    # Affichage du template
+    return render_template('a_propos_manuel.html', page_title="À propos")
+
+# Route pour la page d'à propos sur l'équipe projet "a_propos_equipe.html"
+@app.route('/a-propos/notre-equipe-projet')
+def a_propos_manuel():
+    """
+    Fonction de définition de l'adresse de la page d'à propos sur l'équipe projet "a_propos_equipe.html"
+    Cette page présente l'équipe projet, ses membres et leurs rôles dans le développement de l'application
+    """
+    # Affichage du template
+    return render_template('a_propos_equipe.html', page_title="À propos")
+
+################################
+# LANCEMENT DE L'APPLICATION
+################################
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-""" Contrôler de l'application Flask pour le site web sur les prélèvements d'eau """
-
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/accueil')
-def accueil():
-    return render_template('accueil.html', page_title="Accueil")
-
-@app.route('/tableau-de-bord/carte-des-prelevements')
-def tableau_de_bord():
-    return render_template('carte-des-prelevements.html', page_title="Tableau de bord")
-
-@app.route('/tableau-de-bord/usages-de-l-eau')
-def tableau_de_bord():
-    return render_template('usages-de-l-eau.html', page_title="Tableau de bord")
-
-@app.route('/tableau-de-bord/evolution-temporelle')
-def tableau_de_bord():
-    return render_template('evolution-temporelle.html', page_title="Tableau de bord")
-
-@app.route('/jeux-de-donnees/chroniques')
-def tableau_de_bord():
-    return render_template('jeu-chroniques.html', page_title="Jeux de données")
-
-@app.route('/jeux-de-donnees/ouvrages')
-def tableau_de_bord():
-    return render_template('jeu-ouvrages.html', page_title="Jeux de données")
-
-@app.route('/jeux-de-donnees/points-de-prelevement')
-def tableau_de_bord():
-    return render_template('jeu-points-de-prelevement.html', page_title="Jeux de données")
-
-@app.route('/a-propos/manuel-d-utilisation')
-def tableau_de_bord():
-    return render_template('manuel-d-utilisation.html', page_title="À propos")
-
-@app.route('/a-propos/notre-equipe-projet')
-def tableau_de_bord():
-    return render_template('notre-equipe-projet.html', page_title="À propos")
--- graphiques.py
+++ graphiques.py
@@ -1,17 +1,17 @@
 # -------------- IMPORTATIONS -------------------#
+#! Installer le framework Seaborn depuis le terminal avec la commande : pip install seaborn
 
+import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
-import pandas as pd
-from random import *
 import folium as f
 from folium.plugins import HeatMap, HeatMapWithTime
-import numpy as np
-from model.Chroniques import Chroniques
 from io import BytesIO
 import base64
+from random import *
+from model.chroniques import chroniques
+import model.model as db
 
 # -------------- HISTOGRAMME -------------------#
 
@@ -48,12 +48,6 @@ def sns_pie(data: list, labels: list, titre: str):
     plt.close()
     image_base64 = base64.b64encode(image_stream.getvalue()).decode('utf-8')
     return f'data:image/png;base64,{image_base64}'
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
-import pandas as pd
 
 def sns_courbe_double(data1: list, data2: list, x_values: list, titre: str, x_label: str, y_label: str, label1="Courbe 1", label2="Courbe 2"):
     df1 = pd.DataFrame({'x': x_values, 'y': data1, 'serie': label1})
@@ -121,7 +115,7 @@ labels = ['A', 'B', 'C'] # Nom pour chaque part.
 # sns_pie(data, labels, "Titre")
 # sns_courbe(data, "Titre", "Abscisse", "Ordonnées")
 
-chroniques = Chroniques()
+chroniques = chroniques()
 
 # ---- DIAGRAMME CIRCULAIRE ---- #
 data_usages = chroniques.usage2()
@@ -143,9 +137,7 @@ sns_courbe_double(usage_1, usage_2, chroniques.annee(), "Volume par annee", "Ann
 
 # ----------------- CARTE ---------------------#
 
-chroniques = Chroniques()
-
-# ! J'ai pas réussi à faire celle qui est timed si quelqu'un a la foi de faire
+chroniques = chroniques()
 
 def heatmap(data: np.array, heat: str, map_obj: map):
     """Fonction qui crée une heatmap selon l'argument que l'on souhaite"""
@@ -157,4 +149,33 @@ m = f.Map(location=(49.017561743666164, 6.022989879006374), zoom_start=6)
 
 heatmap(chroniques.donnees(), 'volume', m)
 
-m.save("map.html")
+def map_prelevement(map_obj):
+    """
+    Ajoute les ouvrages sur la carte Folium avec les noms des points de prélèvement associés.
+    """
+    ouvrages = db.obtenir_info_ouvrage()
+    prelevements = db.obtenir_info_prelevement()
+    
+    for _, row in ouvrages.iterrows():
+        lat = row['latitude']
+        lon = row['longitude']
+
+        if pd.notna(lat) and pd.notna(lon):
+            popup_html = f'''
+                <b>{row['nom_ouvrage']}</b><br>
+                Code ouvrage : {row['code_ouvrage']}<br>
+                Département : {row['libelle_departement']}
+            '''
+            popup = f.Popup(popup_html, max_width=150, min_width=50)
+
+            # Taille réduite pour l'icône
+            icon = f.Icon(color='blue', icon='tint', prefix='fa')
+
+            f.Marker(
+                location=(lat, lon),
+                popup=popup,
+                icon=icon
+            ).add_to(map_obj)
+
+m = f.Map(location=(49.017561743666164, 6.022989879006374), zoom_start=6)
+m.save("tab_carte.html")
